ff7152fc3db0aa705342519a7d34fd402cdacca5
SoonHyung_Lee <tnsgud9@naver.com>
2024-02-06 21:18:45 +0900

    project init

---
+++ ApiServer/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiServer.Controllers;
+
+[ApiController]
+[Route("user")]
+public class UserController : ControllerBase
+{
+    [Route("")]
+    [HttpGet]
+    public async Task<IActionResult> HelloWorld()
+    {
+        return Ok("Hello World!"); 
+    }
+}
